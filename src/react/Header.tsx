effe424373e8c8a6f5d5b2527c3ec3bd9de03c40
jakekasan <jake.kasan@gmail.com>
2020-08-18 13:35:29 +0100

    Add correct React typing and text to Home link

--- src/react/Header.tsx
+++ src/react/Header.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import { Link, useLocation } from "react-router-dom";
 
-const Header = () => {
+const Header: FunctionComponent<{}> = () => {
 
     const location = useLocation();
     
     console.log(location.pathname);
-    
+
     return (
         <nav>
-            <Link to="/"></Link>
+            <Link to="/">Home</Link>
             <Link to="/courses">Kurzy</Link>
             <Link to="/projects">Projekty</Link>
             <Link to="/login">Login</Link>
